c21d7ee44357ed049c05280e180efa3a1a1b68f5
unknown <SkYantS28>
2026-09-20 19:14:42 -0300

    adicionando contanstes = colors.ts

---
+++ src/constants/colors.ts
@@ -0,0 +1,9 @@
+export const COLORS = {
+    background: "#F8F4EE",
+    primary: "#7A1F1F",
+    secondary: "#C89B5C",
+    text: "#2D2926",
+    textSecondary: "#6B625A",
+    white: "#FFFFFF",
+    border: "#E5DDD2",
+};
